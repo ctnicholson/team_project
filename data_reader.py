5d28fbd8117ac0bbab3f02abae304c0d4f487162
bfunk1 <bfunk1.personal@gmail.com>
2022-11-23 19:04:41 -0800

    API Works Now

--- data_reader.py
+++ data_reader.py
@@ -7,33 +7,9 @@ headers = {
     'x-rapidapi-key': "95545dabcc3cfa791d8e0f5198ff71b1"
     }
 
-conn.request("GET", "/leagues/?id=1", headers=headers)
+conn.request("GET", "/fixtures/?league=1&season=2022", headers=headers)
 
 res = conn.getresponse()
 data = res.read()
 
 print(data.decode("utf-8"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-# url = "https://soccer.sportmonks.com/api/v2.0/leagues"
-
-# response = requests.get(
-#     'https://soccer.sportmonks.com/api/v2.0/leagues',
-#     params={'api_token': API_KEY}
-# )
-
-# response_data = json.loads(response.text)
-# print(response_data)
-# print(get_data(f"teams/search/USA",{}))
-
