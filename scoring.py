fd7f9722e66f6a662950acb3d1a3148e8f500ccf
Maria Herwagen <112500028+mherwagen@users.noreply.github.com>
2022-12-01 01:37:32 -0500

    Update scoring.py

--- scoring.py
+++ scoring.py
@@ -14,7 +14,8 @@ def get_team_1():
         user_team_1 = "USA"
     games = choose_team(user_team_1)
     if games == []:
-        raise ValueError('This team is not on the roster. Check your spelling! (Capitalization is not required.)')
+        raise ValueError(
+            'This team is not on the roster. Check your spelling! (Capitalization is not required.)')
     print(f'Here are the games and scores for {user_team_1}: {games}.')
     return games, user_team_1
 
@@ -28,23 +29,27 @@ def choose_game():
     for game in games:
         if user_team_2 in game:
             return game, user_team_1, user_team_2
-    raise ValueError('This team is not on the roster. Check your spelling! (Capitalization is not required.)')
+    raise ValueError(
+        'This team is not on the roster. Check your spelling! (Capitalization is not required.)')
 
 
 def create_score_dict():
     game, user_team_1, user_team_2 = choose_game()
-    score_1 = int(input(f"how many points will {user_team_1} end up with? >>> "))
-    score_2 = int(input(f"how many points will {user_team_2} end up with? >>> "))
+    score_1 = int(
+        input(f"how many points will {user_team_1} end up with? >>> "))
+    score_2 = int(
+        input(f"how many points will {user_team_2} end up with? >>> "))
     if score_1 > score_2:
         winner = user_team_1
     elif score_1 < score_2:
         winner = user_team_2
     elif score_1 == score_2:
-        team = input('Predicting a draw means predicting a penatlies round. Which team will win? >>> ')
+        team = input(
+            'Predicting a draw means predicting a penatlies round. Which team will win? >>> ')
         winner = ' '.join(x.capitalize() for x in team.split())
         if winner in ("Usa", "USa", "United States", "US", "Us", "United States Of America"):
             winner = "USA"
-    prediction = {user_team_1 : score_1, user_team_2 : score_2}
+    prediction = {user_team_1: score_1, user_team_2: score_2}
     return game, prediction, winner
 
 
@@ -74,6 +79,7 @@ def get_winner():
                 winner = None
     return real_game, pred_game, winner, pred_winner
 
+
 def get_penalties():
     real_game, pred_game, real_winner, pred_winner = get_winner()
     team_1, team_2 = real_game.keys()
@@ -89,16 +95,12 @@ def compare_scores():
     real_game, pred_game, real_winner, pred_winner, penalties = get_penalties()
     team_1, team_2 = real_game.keys()
     if penalties == True:
-        points +=1
+        points += 1
     if real_game[team_1] == pred_game[team_1] and real_game[team_2] == pred_game[team_2]:
-        points +=1
+        points += 1
     if pred_winner == real_winner:
         points += 1
     return points
-
-
-    
-
 
 
 # print(create_score_dict())
